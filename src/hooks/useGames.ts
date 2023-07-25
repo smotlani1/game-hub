a353a8189de42025e99bd8e62456e3e6a7d0a5c5
Saqib Motlani <motlani.saqib@gmail.com>
2023-07-25 16:03:17 -0500

    Building Game Cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,19 +2,20 @@ import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
-
-  interface Game {
-    id: number;
-    name: string;
-  }
-
-  interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-  }
 
   useEffect(() => {
     const controller = new AbortController();
